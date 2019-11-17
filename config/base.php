7352974e4140e624357379ea2f9d69092221f7d9
WilsonJaya <52271032+WilsonJaya@users.noreply.github.com>
2019-11-17 11:40:38 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => '南寻客的秘密花园',
+  'password' => '2000521tjy',
+  'style' => 'material',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'xjrFJV6110onrfQHAH1*=^*',
+  'client_id' => '0f0580ed-a87f-4517-9885-41196df73390',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'refresh_token' => 'OAQABAAAAAACQN9QBRU3jT6bcBQLZNUj7R6a4wZWyK8XGBlcs-cai-0zcs9uriItVzc9omQXH2_CrNHEkQhgFzyTKeXUxSxG3T35h9xsQYwJvvwknQ6YQbzqEypiKmaySW2ItggwaAvIZWa5Vz7ob6OZTaCb2sCqOwrjtH55TsQPEwdXowYe0t3ZwY4mIInBBeQC22EP4rUYCLX9fA-JPT4_0BWZq6RruS5bHYDYOZ5DowA75yveFIEW97Jshvaz-PC6BdOF9IRNWyHL9Urp1HyJvWVU2PS1C5GKI-NyTfFBPfNrtXamkwi9twXe0-IHm72yqzhvEgi9Lj6zGVujZdcRoUb8_VxoogffTwCZaaiIYqupHXIrA8DDFPAbF5el_3soJdjf_pn4c0ViGtLuRkKSifOtz91FYrQ3pDEL2585Hu7teTqNyaV-J0Ve-h0qs3ZHSukZJgwZspERUlcnYe78oqmmtWYBmMfwb6v9i6KwWU8uJPIgnkr4wNRul-i9734Jc_sPOuRABcU_7_hl623JEmjoRKUCvnkk-GRtk7EcMW6d_4XUtrcvOgDojoKKU4KUq9NnctXfhw8YQzhoYZXb7Lac3N4zcwyEghxe_5DqVa1G53nN8oiG8omcUo59wI0U6U_9iSRSGHqXbtRnnVFUMjbxUMC82nN9-88PJf9zqe_d2iIgJ7aQg1Nrlq3zvix3UO4AzELsOAivu_PapYptkOhrgiZAtRicYL1vuF5suqBAHu3_mewVXFUse34LvXvnGGapUbmvSitAlmBOQLXhI54n9xx23jqFvYT6neotmjAlBr01_2qY_ooABnk8IRM5axD1WVYVboC6Hc3p_2cOCKtsNWcL4IAA',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
